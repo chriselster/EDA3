815b0e1920d098edb605edcf75c551f5b8a4d2db
Christian Elster <christian.elster0@gmail.com>
2018-02-13 16:02:24 -0300

    Tentativa de heap 1
    Duas coisas: 
    -Tem um caracter misterioso que não aparece nenhuma vez q tá ficando em primeiro mesmo com 0.
    -Ainda tem q mudar pra min heapfy só que aí vai ficar uns 100 caracteres com 0 na frente pra gente lidar

--- compactador.c
+++ compactador.c
@@ -1,20 +1,55 @@
 #include <stdio.h>
 
+typedef struct no
+{
+	char type;
+	int qtd;
+}no;
+
+void min_heapfy(no v[129], int n, int i){
+	int j = i;
+	while(2*j <= n){
+		int f = 2*j;
+		if (f<=n && v[f].qtd<v[f+1].qtd){
+			f = f+1;
+		}
+		printf("%d, %d?\n", f,j);
+		if (v[j].qtd>=v[f].qtd){
+			j=n;
+		}else{
+			no aux;
+			aux = v[f];
+			v[f] = v[j];
+			v[j] = aux;
+			j = f;
+		}
+	}
+	return;
+}
+
+
+
 int main(){
 	char a;
-	int v[128];
+	no v[129];
 
-	for (int i = 0; i < 128; ++i)
+	for (int i = 0; i < 129; ++i)
 	{
-		v[i]=0;
+		v[i].type=i-1;
+		v[i].qtd=0;
 	}
 
 	while(scanf("%c", &a) != EOF){
-		v[a]++;
+		v[a+1].qtd++;
 	}
 	
-	for (int i = 0; i < 128; ++i)
+	for (int i = 129/2; i > 1; --i)
 	{
-		printf("%c: %d\n", i, v[i]);
+		min_heapfy(v,129,i);
+	}
+
+	for (int i = 1; i < 129; ++i)
+	{
+		printf("%c: %d\n",v[i].type, v[i].qtd);
 	}
 }
